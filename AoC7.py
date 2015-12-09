1a0441c0c9fc6daf3f1f878c99ebc03bcdc3bdfb
Noah Rosamilia <ivoahivoah@gmail.com>
2015-12-09 10:16:57 -0500

    Initial Day 7 solution (still broken)

--- AoC7.py
+++ AoC7.py
@@ -1,4 +1,40 @@
 input = open('7.in').readlines()
 
-for line in input:
-    
+circuit = {}
+
+def signal(circuit, wire):
+	try:
+		return int(wire)
+	except ValueError:
+		try:
+			if circuit[wire]['gate'] == 'NONE':
+				return signal(circuit, circuit[wire]['inputs'][0])
+			elif circuit[wire]['gate'] == 'AND':
+				return signal(circuit, circuit[wire]['inputs'][0]) & signal(circuit, circuit[wire]['inputs'][1])
+			elif circuit[wire]['gate'] == 'OR':
+				return signal(circuit, circuit[wire]['inputs'][0]) | signal(circuit, circuit[wire]['inputs'][1])
+			elif circuit[wire]['gate'] == 'NOT':
+				return ~signal(circuit, circuit[wire]['inputs'][0])
+			elif circuit[wire]['gate'] == 'LSHIFT':
+				return signal(circuit, circuit[wire]['inputs'][0]) << signal(circuit, circuit[wire]['inputs'][1])
+			elif circuit[wire]['gate'] == 'RSHIFT':
+				return signal(circuit, circuit[wire]['inputs'][0]) >> signal(circuit, circuit[wire]['inputs'][1])
+		except TypeError as e:
+			print wire, circuit[wire]
+			raise e
+
+for i, line in enumerate(input):
+	line = line.split()
+	line.pop(-2)
+	wire = line.pop(-1)
+	inputs = []
+	gate = 'NONE'
+	for i in line:
+		if i.upper() == i:
+			gate = i
+		else:
+			inputs.append(i)
+
+	circuit[wire] = {'gate': gate, 'inputs': inputs, 'line': i}
+
+print signal(circuit, 'a')
