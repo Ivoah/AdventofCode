ca1ceb0dd3f96e6de241dc6bb405df82372ba126
Noah Rosamilia <ivoahivoah@gmail.com>
2015-12-13 09:07:49 -0500

    Add value caching for day 7

--- AoC7.py
+++ AoC7.py
@@ -1,33 +1,39 @@
 input = open('7.in').readlines()
-#input = ['123 -> x',
-#		 '456 -> y',
-#		 'x AND y -> d',
-#		 'x OR y -> e',
-#		 'x LSHIFT 2 -> f',
-#		 'y RSHIFT 2 -> g',
-#		 'NOT x -> h',
-#		 'NOT y -> i'
-#]
+'''
+input = ['123 -> x',
+		 '456 -> y',
+		 'x AND y -> d',
+		 'x OR y -> e',
+		 'x LSHIFT 2 -> f',
+		 'y RSHIFT 2 -> g',
+		 'NOT x -> h',
+		 'NOT y -> i'
+]
+'''
 
 gates = ['AND', 'OR', 'NOT', 'LSHIFT', 'RSHIFT']
 circuit = {}
 
+cache = {}
 def signal(circuit, wire):
 	try:
 		return int(wire)
 	except ValueError:
-		if circuit[wire]['gate'] == 'NONE':
-			return signal(circuit, circuit[wire]['inputs'][0])
-		elif circuit[wire]['gate'] == 'AND':
-			return signal(circuit, circuit[wire]['inputs'][0]) & signal(circuit, circuit[wire]['inputs'][1])
-		elif circuit[wire]['gate'] == 'OR':
-			return signal(circuit, circuit[wire]['inputs'][0]) | signal(circuit, circuit[wire]['inputs'][1])
-		elif circuit[wire]['gate'] == 'NOT':
-			return 65535 - signal(circuit, circuit[wire]['inputs'][0])
-		elif circuit[wire]['gate'] == 'LSHIFT':
-			return signal(circuit, circuit[wire]['inputs'][0]) << signal(circuit, circuit[wire]['inputs'][1])
-		elif circuit[wire]['gate'] == 'RSHIFT':
-			return signal(circuit, circuit[wire]['inputs'][0]) >> signal(circuit, circuit[wire]['inputs'][1])
+		global cache
+		if wire not in cache.values():
+			if circuit[wire]['gate'] == 'NONE':
+				cache[wire] = signal(circuit, circuit[wire]['inputs'][0])
+			elif circuit[wire]['gate'] == 'AND':
+				cache[wire] = signal(circuit, circuit[wire]['inputs'][0]) & signal(circuit, circuit[wire]['inputs'][1])
+			elif circuit[wire]['gate'] == 'OR':
+				cache[wire] = signal(circuit, circuit[wire]['inputs'][0]) | signal(circuit, circuit[wire]['inputs'][1])
+			elif circuit[wire]['gate'] == 'NOT':
+				cache[wire] = 65535 - signal(circuit, circuit[wire]['inputs'][0])
+			elif circuit[wire]['gate'] == 'LSHIFT':
+				cache[wire] = signal(circuit, circuit[wire]['inputs'][0]) << signal(circuit, circuit[wire]['inputs'][1])
+			elif circuit[wire]['gate'] == 'RSHIFT':
+				cache[wire] = signal(circuit, circuit[wire]['inputs'][0]) >> signal(circuit, circuit[wire]['inputs'][1])
+		return cache[wire]
 
 for ln, line in enumerate(input):
 	line = line.split()
