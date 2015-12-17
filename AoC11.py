117330597adcf7d5bcdee1bfbc1dedd053d53e00
Noah Rosamilia <ivoahivoah@gmail.com>
2015-12-17 14:53:21 -0500

    Finish day 11

--- AoC11.py
+++ AoC11.py
@@ -1,4 +1,5 @@
 input = 'hxbxwxba'
+#input = 'abcdefgh'
 
 def incString(string):
     string = list(string)
@@ -15,7 +16,42 @@ def incString(string):
     return ''.join(string)
 
 def isValid(password):
-    if len(password) != 8 or password.lower() != password:
-        return False
+    if len(password) != 8 or password.lower() != password: return False
+
+    n = 0
+    lc = '\x00'
+    v = False
+    for c in password:
+        if ord(lc) + 1 == ord(c):
+            n += 1
+        else:
+            n = 0
+        lc = c
+        if n == 2: v = True
+    if not v: return False
+
+
+    lc = ''
+    n = 0
+    a = ''
+    for c in password:
+        if lc == c and a != c:
+            n += 1
+            a = c
+        lc = c
+    if n < 2: return False
+
+    any(c in password for c in 'iol')
+    for c in 'iol':
+        if c in password: return False
 
     return True
+
+while not isValid(input):
+    input = incString(input)
+print input
+
+input = incString(input)
+while not isValid(input):
+    input = incString(input)
+print input
