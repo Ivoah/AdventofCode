1818918d828e58f47c1914d581e428fde4f77102
Noah Rosamilia <ivoahivoah@gmail.com>
2015-12-17 20:57:07 -0500

    Add day 12

---
+++ AoC12.py
@@ -0,0 +1,16 @@
+import json
+
+input = json.load(open('12.in'))
+
+def count(j):
+    if type(j) == int:
+        return j
+    elif type(j) == list:
+        return sum([count(v) for v in j])
+    elif type(j) == dict:
+        if 'red' in j.values(): return 0
+        return sum([count(v) for v in j.values()])
+    else:
+        return 0
+
+print count(input)
