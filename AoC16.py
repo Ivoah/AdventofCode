68b505cd0a42d9c83dd1075709b949eb8f8098b4
Noah Rosamilia <ivoahivoah@gmail.com>
2016-04-26 17:44:20 -0400

    Add day 16 solution

---
+++ AoC16.py
@@ -0,0 +1,27 @@
+input = open('16.in').readlines()
+
+aunts = []
+target = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
+for i, line in enumerate(input):
+    line = line.split()[2:]
+    aunts.append({})
+    for pair in zip(line[::2], line[1::2]):
+        aunts[i][pair[0][:-1]] = int(pair[1].strip(','))
+
+for i, aunt in enumerate(aunts):
+    for j, item in enumerate(aunt):
+        if aunt[item] != target[item]:
+            break
+        if j == len(aunt) - 1: print i + 1
+
+for i, aunt in enumerate(aunts):
+    for j, item in enumerate(aunt):
+        if item in ['cats', 'trees']:
+            if aunt[item] <= target[item]:
+                break
+        elif item in ['pomeranians', 'goldfish']:
+            if aunt[item] >= target[item]:
+                break
+        elif aunt[item] != target[item]:
+            break
+        if j == len(aunt) - 1: print i + 1
