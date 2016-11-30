1e377323507801ddaa30e3ef0a96eaf80eab84fe
Noah Rosamilia <ivoahivoah@gmail.com>
2016-11-30 08:49:12 -0500

    Rearrange files

--- 2015/AoC20.py
+++ 2015/AoC20.py
@@ -24,3 +24,13 @@ for n, house in enumerate(houses):
     if house >= target:
         print n
         break
+
+houses = [0 for i in range(target/10)]
+for i in range(1, target / 10):
+    for j in range(i, (i*50 + i) if (i*50 + i) < target/10 else target/10, i):
+        houses[j] += i * 10
+
+for n, house in enumerate(houses):
+    if house >= target:
+        print n
+        break
