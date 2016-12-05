bc1c72fc365edbd660fe1e149970ad18c216cd76
Noah Rosamilia <ivoahivoah@gmail.com>
2016-12-05 00:26:35 -0500

    Put part 1 solution back

--- 2016/AoC5.py
+++ 2016/AoC5.py
@@ -1,15 +1,20 @@
 from hashlib import md5
 
 door_id = 'wtnhxymk'
-password = ['_']*8
+password1 = ''
+password2 = ['_']*8
 
 index = 0
-while '_' in password:
+while '_' in password2:
     d = ''
     while d[:5] != '00000':
         d = md5(bytes(door_id + str(index), 'ascii')).hexdigest()
         index += 1
-    if d[5] in '01234567' and password[int(d[5])] == '_':
-        password[int(d[5])] = d[6]
-        
-print(''.join(password))
+
+    if len(password1) < 8:
+        password1 += d[5]
+    if d[5] in '01234567' and password2[int(d[5])] == '_':
+        password2[int(d[5])] = d[6]
+
+print(password1)
+print(''.join(password2))
