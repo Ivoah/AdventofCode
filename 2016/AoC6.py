672d81acf64a16ea3ab86728cfdb5fef83c07294
Noah Rosamilia <ivoahivoah@gmail.com>
2016-12-06 00:11:57 -0500

    Add day 6 solution

---
+++ 2016/AoC6.py
@@ -0,0 +1,12 @@
+import collections
+
+data = open('6.in').read()
+
+msg1 = ''
+msg2 = ''
+for i in range(8):
+    c = collections.Counter(data[i::9]).most_common()
+    msg1 += c[0][0]
+    msg2 += c[-1][0]
+
+print(msg1, msg2)
