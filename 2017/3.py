51b48be19c368a445c599522e12ea27710a5d769
Noah Rosamilia <ivoahivoah@gmail.com>
2017-12-03 12:10:32 -0500

    Day 3 part 1 (thanks Dalton!)

---
+++ 2017/3.py
@@ -0,0 +1,26 @@
+from math import *
+
+INPUT = 289326
+
+nearest_odd_square = sq = lambda n: (floor(sqrt(n)) if floor(sqrt(n))%2 != 0 else floor(sqrt(n)) - 1)**2
+
+n = INPUT - 1
+p = nearest_odd_square(n)
+
+x = (sqrt(p) + 1)/2
+y = (sqrt(p) - 1)/2
+
+if p + sqrt(p) >= n:
+    y -= n - p
+elif p + 2*sqrt(p) + 1 >= n:
+    y -= sqrt(p)
+    x -= n - p - sqrt(p)
+elif p + 3*sqrt(p) + 3 >= n:
+    y -= n - p - 3*sqrt(p) - 2
+    x = -(sqrt(p) + 1)/2
+else:
+    y += 1
+    x -= p + 4*sqrt(p) - n
+
+print(f'({x}, {y})')
+print(abs(x) + abs(y))
