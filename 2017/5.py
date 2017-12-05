9e905d1e39759f31653d3399ea4cd8dcf4408ef9
Noah Rosamilia <ivoahivoah@gmail.com>
2017-12-05 10:58:16 -0500

    Day 5 part 2

--- 2017/5.py
+++ 2017/5.py
@@ -9,3 +9,15 @@ while 0 <= pc < len(jumps):
     n += 1
 
 print(n)
+
+jumps = list(map(int, open('5.in').readlines()))
+
+n = 0
+pc = 0
+while 0 <= pc < len(jumps):
+    jump = jumps[pc]
+    jumps[pc] += -1 if jump >= 3 else 1
+    pc += jump
+    n += 1
+
+print(n)
