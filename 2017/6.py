62c27ad2ce1e4cc09cb5c95be6b620546c844c2c
Noah Rosamilia <ivoahivoah@gmail.com>
2017-12-06 00:41:56 -0500

    Day 6 both parts

---
+++ 2017/6.py
@@ -0,0 +1,18 @@
+banks = list(map(int, open('6.in').read().split()))
+
+prev = [banks[:]]
+n = 0
+
+while True:
+    bank = banks.index(max(banks))
+    redistrib = banks[bank]
+    banks[bank] = 0
+    for i in range(bank + 1, redistrib + bank + 1):
+        banks[i%len(banks)] += 1
+
+    if banks in prev:
+        print(len(prev))
+        print(len(prev) - prev.index(banks))
+        break
+
+    prev.append(banks[:])
