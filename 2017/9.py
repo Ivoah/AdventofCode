d5756d76bffdb79ccf6feb87dffd595ff0383f28
Noah Rosamilia <ivoahivoah@gmail.com>
2017-12-09 00:18:44 -0500

    Day 9 both parts

---
+++ 2017/9.py
@@ -0,0 +1,27 @@
+stream = open('9.in').read()
+
+total_score = 0
+current_score = 0
+garbage = 0
+
+i = 0
+while i < len(stream):
+    c = stream[i]
+    if c == '{':
+        current_score += 1
+    elif c == '}':
+        total_score += current_score
+        current_score -= 1
+    elif c == '<':
+        while stream[i] != '>':
+            garbage += 1
+            if stream[i] == '!':
+                garbage -= 1
+                i += 1
+            i += 1
+        garbage -= 1
+
+    i += 1
+
+print(total_score)
+print(garbage)
