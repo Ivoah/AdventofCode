2f2514eb34c8e23c7951111b9b99946162e00f8b
Noah Rosamilia <ivoahivoah@gmail.com>
2018-12-02 02:10:53 -0500

    Add day 1

---
+++ 2018/1.py
@@ -0,0 +1,14 @@
+print(eval(''.join(open('1.in').read().split())))
+
+freqs = {0: True}
+freq = 0
+changes = list(map(int, open('1.in').read().split()))
+
+c = 0
+while True:
+    freq += changes[c%len(changes)]
+    if freq in freqs:
+        print(freq)
+        break
+    freqs[freq] = True
+    c += 1
