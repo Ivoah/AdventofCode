353d72050e51163970c60db66c5b1cf9eb54b36f
Noah Rosamilia <ivoahivoah@gmail.com>
2020-12-02 10:13:15 -0500

    2020 day 2

---
+++ 2020/2.py
@@ -0,0 +1,21 @@
+import re
+
+patt = re.compile(r'(\d+)-(\d+) (\w): (\w+)')
+
+with open('2.in') as f:
+    lines = f.readlines()
+
+valid = 0
+for line in lines:
+    if match := patt.match(line):
+        if int(match.group(1)) <= match.group(4).count(match.group(3)) <= int(match.group(2)):
+            valid += 1
+print(valid)
+
+valid = 0
+for line in lines:
+    if match := patt.match(line):
+        p0, p1, c, pw = int(match.group(1)) - 1, int(match.group(2)) - 1, match.group(3), match.group(4)
+        if [pw[p0], pw[p1]].count(c) == 1:
+            valid += 1
+print(valid)
---
+++ 2020/2.scala
@@ -0,0 +1,12 @@
+import scala.io.Source
+
+val patt = raw"(\d+)-(\d+) (\w): (\w+)".r
+
+val lines = Source.fromFile("2.in").getLines().toSeq
+println(lines.count {
+  case patt(min, max, c, pw) => (min.toInt to max.toInt).contains(pw.count(_ == c.head))
+})
+
+println(lines.count {
+  case patt(p0, p1, c, pw) => Seq(pw(p0.toInt - 1), pw(p1.toInt - 1)).count(_ == c.head) == 1
+})
