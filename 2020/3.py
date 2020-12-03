35f08c86f8235daf7b33788d3dda548d4110558e
Noah Rosamilia <ivoahivoah@gmail.com>
2020-12-03 08:22:24 -0500

    2020 day 3

---
+++ 2020/3.py
@@ -0,0 +1,14 @@
+import math
+
+with open('3.in') as f:
+    _map = f.readlines()
+
+def trees(slope):
+    t = 0
+    for i, y in enumerate(range(0, len(_map), slope[1])):
+        if _map[y][i*slope[0]%len(_map[y].strip())] == '#':
+            t += 1
+    return t
+
+print(trees((3, 1)))
+print(math.prod(trees(slope) for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))
---
+++ 2020/3.scala
@@ -0,0 +1,7 @@
+import scala.io.Source
+
+val map = Source.fromFile("3.in").getLines().toSeq
+
+def trees(slope: (Int, Int)): Long = (map.indices by slope._2).zipWithIndex.count{case (y, i) => map(y)(i*slope._1%map(y).length) == '#'}
+println(trees((3, 1)))
+println(Seq((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)).map(trees).product)
