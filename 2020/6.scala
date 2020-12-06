3bdeada4ef970113e87e93bc70db5668c34c6fab
Noah Rosamilia <ivoahivoah@gmail.com>
2020-12-06 00:33:57 -0500

    2020 day 6

--- 2020/6.scala
+++ 2020/6.scala
@@ -1,5 +1,5 @@
 import scala.io.Source
 
 val answers = Source.fromFile("6.txt").mkString.split("\n\n").map(_.split("\\s").map(_.toSet))
-println(answers.map(_.reduce((s1, s2) => s1 union s2).size).sum)
-println(answers.map(_.reduce((s1, s2) => s1 intersect s2).size).sum)
+println(answers.map(_.reduce(_ union _).size).sum)
+println(answers.map(_.reduce(_ intersect _).size).sum)
