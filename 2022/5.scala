45261ab3c6aa8014a19ba321370aa50b001bfaab
Noah Rosamilia <ivoahivoah@gmail.com>
2022-12-06 12:54:12 -0500

    2022 day 5

---
+++ 2022/5.scala
@@ -0,0 +1,29 @@
+@main
+def main() = {
+  val stacks = io.Source.fromFile("4.txt").getLines().takeWhile(_.nonEmpty).toSeq.dropRight(1).map(_.grouped(4).map(_(1)).toSeq).transpose.map(_.filter(_ != ' '))
+    
+  val move_re = raw"move (\d+) from (\d+) to (\d+)".r
+  val moves = io.Source.fromFile("4.txt").getLines().collect {
+    case move_re(num, from, to) => (num.toInt, from.toInt - 1, to.toInt - 1)
+  }.toSeq
+
+  val final_stacks = moves.foldLeft(stacks) { case (current_stacks, (num, from, to)) =>
+    current_stacks.zipWithIndex.map { case (stack, i) =>
+      if (i == from) stack.drop(num)
+      else if (i == to) current_stacks(from).take(num).reverse ++ stack
+      else stack
+    }
+  }
+
+  println(final_stacks.map(_.head).mkString)
+
+  val final_stacks_part_2 = moves.foldLeft(stacks) { case (current_stacks, (num, from, to)) =>
+    current_stacks.zipWithIndex.map { case (stack, i) =>
+      if (i == from) stack.drop(num)
+      else if (i == to) current_stacks(from).take(num) ++ stack
+      else stack
+    }
+  }
+
+  println(final_stacks_part_2.map(_.head).mkString)
+}
