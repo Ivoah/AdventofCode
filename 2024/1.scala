9efd7f1876c76bc7b3881bf329205dc109470e1f
Noah Rosamilia <noah@ivoah.net>
2024-12-01 00:10:51 -0500

    Day 1 2024

---
+++ 2024/1.scala
@@ -0,0 +1,16 @@
+import scala.io.Source
+
+@main
+def main() = {
+  val (l1, l2) = Source.fromFile("1.txt")
+    .getLines
+    .toSeq
+    .map(_.split(raw"\s+") match { case Array(a, b) => (a.toInt, b.toInt) })
+    .unzip
+  
+  val distance = l1.sorted.zip(l2.sorted).map { case (a, b) => math.abs(b - a) }.sum
+  println(distance)
+
+  val similarity = l1.map(n => n * l2.count(_ == n)).sum
+  println(similarity)
+}
