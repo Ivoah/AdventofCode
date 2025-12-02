94248a0db1d84e467fd378cd5ae5c901f0580c3b
Noah Rosamilia <noah.rosamilia@grantstreet.com>
2025-12-02 14:50:25 -0500

    Add clojure solution for day 2

---
+++ 2025/2025-02.clj
@@ -0,0 +1,13 @@
+(require '[clojure.string :as str])
+
+(def toLong #(Long/parseLong %))
+(defn parseRange [s]
+  (let [ss (map toLong (str/split s #"-"))]
+    (range (nth ss 0) (+ (nth ss 1) 1))))
+
+(def ids (mapcat
+  parseRange
+  (str/split (str/trim (slurp "2.txt")) #",")))
+
+(println (apply + (filter #(re-matches #"(\d+)\1" (str %)) ids)))
+(println (apply + (filter #(re-matches #"(\d+)\1+" (str %)) ids)))
